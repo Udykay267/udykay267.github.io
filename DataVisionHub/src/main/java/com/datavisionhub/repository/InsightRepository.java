705f54cbe80882700bfde4e37125668a1859c791
Uduak Attah Unanam <uduak618@gmail.com>
2025-06-19 02:46:31 +0100

    Delete DataVisionHub directory

--- DataVisionHub/src/main/java/com/datavisionhub/repository/InsightRepository.java
+++
@@ -1,9 +0,0 @@
-package com.datavisionhub.repository;
-
-import com.datavisionhub.model.Insight;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface InsightRepository extends JpaRepository<Insight, Long> {
-    List<Insight> findByMetricContainingIgnoreCase(String metric);
-}
